c46f87ff182e7481789149138c18ec7629311df8
yuki-mt <yuki1031mt@gmail.com>
2019-02-25 16:54:09 +0900

    fix

--- git_test.py
+++ git_test.py
@@ -4,18 +4,17 @@ Githubをpythonから操作するサンプルコード
 pip install -U pip && pip install pygithub
 """
 from github import Github
-from github.InputGitTreeElement import InputGitTreeElement
 from github.GithubException import GithubException
 import configparser
 import sys
-import base64
 
 
 WORKING_BRANCH = 'feature/test'
 TARGET_BRANCH = 'master'
-BASE_URL = 'https://git-dev.linecorp.com/api/v3'
+BASE_URL = 'https://github.com/api/v3'
 IS_SUCCESS_KEY = 'success'
 SUCCESS_RESULT = {IS_SUCCESS_KEY: True}
+
 
 def main():
     config = configparser.ConfigParser()
@@ -29,22 +28,22 @@ def main():
     config.read('config/git_token.conf')
     ACCESS_TOKEN = config['default']['access_token']
     ORG = 'yuki-mt'
-    skill = 'scripts'
-    repo_path = "%s/%s" % (ORG, skill)
+    REPO_NAME = 'scripts'
+    repo_path = f"{ORG}/{REPO_NAME}"
 
     try:
         g = Github(ACCESS_TOKEN, base_url=BASE_URL)
         # git checkout
         repo = g.get_repo(repo_path)
-        branch = repo.get_branch(WORKING_BRANCH)
+        # branch = repo.get_branch(WORKING_BRANCH)
     except GithubException as e:
         print(type(e.data))
         msg = "Failed to get branch\n{0}(err={1})".format(e.data, e.status)
         sys.stderr.write(msg)
         return -1
 
-    #print(delete_file(repo, 'sample.txt'))
-    #print(edit_file(repo, 'sample.txt', 'aaafea'))
+    # print(delete_file(repo, 'sample.txt'))
+    # print(edit_file(repo, 'sample.txt', 'aaafea'))
     print(merge(repo))
 
 
@@ -54,11 +53,11 @@ def edit_file(repo, path, content, comment='update a file'):
 
     try:
         current_content = repo.get_file_contents(path, WORKING_BRANCH).content
-    except GithubException as e:
+    except GithubException:
         current_content = False
 
     try:
-        if current_content == False:
+        if not current_content:
             result = repo.create_file(path, comment, content, WORKING_BRANCH)
         else:
             blob = repo.create_git_blob(current_content, 'base64')
@@ -70,6 +69,7 @@ def edit_file(repo, path, content, comment='update a file'):
         msg = "Failed to update file\n{0}(err={1})".format(e.data, e.status)
         sys.stderr.write(msg)
         return get_error_json(msg)
+
 
 def delete_file(repo, path, comment='delete a file'):
     if not path.startswith('/'):
@@ -85,6 +85,7 @@ def delete_file(repo, path, comment='delete a file'):
         sys.stderr.write(msg)
         return get_error_json(msg)
 
+
 def merge(repo):
     try:
         commit = repo.merge(TARGET_BRANCH, WORKING_BRANCH)
@@ -98,8 +99,7 @@ def merge(repo):
 
 
 def get_error_json(msg):
-   return {'message': msg, IS_SUCCESS_KEY: False}
-
+    return {'message': msg, IS_SUCCESS_KEY: False}
 
 
 if __name__ == "__main__":
