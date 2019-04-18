029e9aa6f2915457ecad4d4a092dbe5bbb2e09d4
yuki-mt <yuki1031mt@gmail.com>
2019-04-19 00:31:09 +0900

    fix

--- setup/termux.sh
+++ setup/termux.sh
@@ -7,20 +7,26 @@
 ###
 
 ## 1.
-apt update && apt upgrade
-pkg install -y sed vim-python curl proot clang make python-dev libcurl-dev libcrypt-dev openssl-dev ruby
-apt install -y coreutils nodejs ruby-dev libxml2-dev libxslt-dev pkg-config make clang
-npm install -y -g n
+pkg upgrade
+
+pkg install -y zsh
 bash -c "$(curl -fsSL https://git.io/oh-my-termux)"
+
 
 ## 2.
 # vim ~/.oh-my-zsh/plugins/git/git.plugin.zsh  # git のalias 削除
 
 ## 3.
+# cp ../files/termux/zshrc ~/.zshrc
+# source ~/.zshrc
+
 # mkdir ~/workspace
-# pkg install -y fzf tmux openssh
+
+# pkg install -y sed vim-python curl proot clang make python-dev libcurl-dev libcrypt-dev openssl-dev ruby fzf tmux openssh
+# apt install -y coreutils nodejs ruby-dev libxml2-dev libxslt-dev pkg-config make clang
+
 # ssh-keygen
-# 
+
 # ./git.sh
 # cp ../files/termux/tmux.conf ~/.tmux.conf
 # 
@@ -28,10 +34,16 @@ bash -c "$(curl -fsSL https://git.io/oh-my-termux)"
 # mkdir ~/.ctags.d
 # cp ../files/ctags ~/.ctags.d/config.ctags
 # cp -R ../files/dict ~/.vim/
-# 
-# cp ../files/termux/zshrc ~/.zshrc
-# source ~/.zshrc
+
+
 # pip install -U pip
 # pip install flake8 mypy
+# curl -L https://its-pointless.github.io/setup-pointless-repo.sh | sh
+# apt install -y freetype freetype-dev libpng libpng-dev pkg-config libzmq libzmq-dev
+# pkg install -y libjpeg-turbo-dev libjpeg-turbo-progs
+# pkg install -y scipy
+# LDFLAGS=" -lm -lcompiler_rt" pip install jupyter pandas matplotlib
+
 # termux-chroot
+# npm install -y -g n
 # n latest
