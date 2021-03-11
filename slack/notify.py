6de147857b470c5c1217757374db0c62c5391026
yuki-mt <yuki1032mt@gmail.com>
2021-03-12 01:13:55 +0900

    fix

--- slack/notify.py
+++ slack/notify.py
@@ -1,16 +1,15 @@
 import json
 import urllib.request
 
-headers = {
-    'Content-Type': 'application/json',
-}
 # how to create URL: https://api.slack.com/messaging/webhooks
-webhook_url = ('https://hooks.slack.com/services/xxx/xxx/xxx')
+webhook_url = 'https://hooks.slack.com/services/xxx/xxx/xxx'
 payload = {
+    "channel": "#general",
+    "username": "webhookbot",
+    "icon_emoji": ":ghost:",
     'text': "<https://example.com|Overlook Hotel>\n:x: :ok: "
 }
 req = urllib.request.Request(webhook_url,
-                             json.dumps(payload).encode(),
-                             headers)
+                             json.dumps(payload).encode())
 with urllib.request.urlopen(req) as res:
     body = res.read()
