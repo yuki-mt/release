ff03cf91ac1ba52cb2abde7c1e8262a582989d0c
yuki-mt <yuki1032mt@gmail.com>
2021-05-28 11:20:37 +0900

    fix

--- files/sh-plugin/ssh.zsh
+++ files/sh-plugin/ssh.zsh
@@ -1,12 +1,27 @@
 alias ssh='ssh -o ServerAliveInterval=60'
 
 syn () {
-  rsync --exclude='.mypy_cache' --exclude='.git' --exclude='*_venv' --exclude='node_modules' --exclude='__pycache__' \
-    -zzav `git rev-parse --show-toplevel`/ matoba.us-central1-a.newagent-4664b:/home/u0_a84/workspace/$(basename `git rev-parse --show-toplevel`)/
+  public_ip=`gcloud compute instances describe instance-1 \
+    --zone "us-central1-a" \
+    --project "..." \
+    --format='get(networkInterfaces[0].accessConfigs[0].natIP)'`
+  rsync --exclude='.mypy_cache' --exclude='.git' --exclude='.venv' \
+    --exclude='node_modules' --exclude='__pycache__' \
+    -e "ssh -i ~/.ssh/google_compute_engine" \
+    -zzav `git rev-parse --show-toplevel`/ \
+    ymatoba@$public_ip:/home/ymatoba/workspace/$(basename `git rev-parse --show-toplevel`)/
 }
 syn-del () {
-  rsync --exclude='.mypy_cache' --exclude='.git' --exclude='*_venv' --exclude='node_modules' --exclude='__pycache__' \
-    --delete -zzav `git rev-parse --show-toplevel`/ matoba.us-central1-a.newagent-4664b:/home/u0_a84/workspace/$(basename `git rev-parse --show-toplevel`)/
+  public_ip=`gcloud compute instances describe instance-1 \
+    --zone "us-central1-a" \
+    --project "..." \
+    --format='get(networkInterfaces[0].accessConfigs[0].natIP)'`
+  rsync --exclude='.mypy_cache' --exclude='.git' --exclude='.venv' \
+    --exclude='node_modules' --exclude='__pycache__' \
+    --delete
+    -e "ssh -i ~/.ssh/google_compute_engine" \
+    -zzav `git rev-parse --show-toplevel`/ \
+    ymatoba@$public_ip:/home/ymatoba/workspace/$(basename `git rev-parse --show-toplevel`)/
 }
 pf () {
   ssh -fNL $1:10.128.0.5:$1 matoba.us-central1-a.newagent-4664b
