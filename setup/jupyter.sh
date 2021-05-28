cf3f5bd9db487ea598e61d3e1759dce242be533f
yuki-mt <yuki1032mt@gmail.com>
2021-05-28 10:13:24 +0900

    fix

--- setup/jupyter.sh
+++ setup/jupyter.sh
@@ -1,7 +1,12 @@
-mkdir -p $(jupyter --data-dir)/nbextensions
-cd $(jupyter --data-dir)/nbextensions
+# enable Hinterland in Nbextentions
+poetry add jupyter-contrib-nbextensions jupyter-nbextensions-configurator
+poetry run jupyter contrib nbextension install
+poetry run jupyter nbextensions_configurator enable
+
+mkdir -p $(poetry run jupyter --data-dir)/nbextensions
+cd $(poetry run jupyter --data-dir)/nbextensions
 git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
-jupyter nbextension enable vim_binding/vim_binding
+poetry run jupyter nbextension enable vim_binding/vim_binding
 
 mkdir -p ~/.jupyter/custom
 cp ../files/custom.js ~/.jupyter/custom
